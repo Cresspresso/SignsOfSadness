80f5d5c71225072bda4eff5bddfd5a41372d469b
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-08-14 16:36:04 +1200

    Basic Random Script

---
+++ SignsOfSadness/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour {
+
+    public int MinRange = 0;
+    public int MaxRange = 1;
+    public float MinSpawnTime = 1.0f;
+    public float MaxSpawnTime = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("WordSpawn", Random.Range(MinSpawnTime, MaxSpawnTime));
+	}
+	
+    void WordSpawn () {
+        int WordRange = Random.Range(MinRange, MaxRange);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
