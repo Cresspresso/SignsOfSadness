09fbc6d08e7c57cd15b748128d2efe2bb527ca63
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-08-14 16:00:04 +1200

    mg2

---
+++ SignsOfSadness/Assets/Scenes/ExampleMergeConflict.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMergeConflict : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		var x = "PLEASE DO ME";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
