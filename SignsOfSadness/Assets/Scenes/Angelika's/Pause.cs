aad1de5f6ad2aff18859209772375a05143e73c1
1Angelika <53497909+1Angelika@users.noreply.github.com>
2019-08-16 13:18:27 +1200

    .

---
+++ SignsOfSadness/Assets/Scenes/Angelika's/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour 
+{
+    bool isPaused = false; 
+
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+    
+    public void resumeGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+    }
+ 
+}
+
