29015e741015f08c86fc542c399bc3e7c5fee9cb
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-08-21 09:48:40 +1200

    Bad Words Added

--- SignsOfSadness/Assets/Scenes/GameController.cs
+++ SignsOfSadness/Assets/Scenes/GameController.cs
@@ -12,6 +12,7 @@ public class GameController : MonoBehaviour
     public GameObject BadTextPrefab;
     public Transform TextParent;
 
+    private int Bias = 0;
     private static readonly string[] goodWords = @"absolutely
 accepted
 achievement
@@ -50,6 +51,42 @@ stunning
 smile
 special
 terrific".Split('\n');
+    private static readonly string[] badWords = @"angry
+annoy
+awful
+broken
+boring
+beneath
+bad
+collapse
+can't
+cold-hearted
+corrupt
+cry
+damaging
+depressed
+damage
+dead
+disgusting
+fear
+feeble
+fail
+faulty
+greed
+gruesome
+horrible
+hurtful
+hostile
+immature
+imperfect
+jealous
+petty
+sickening
+terrible
+unwanted
+ugly
+unpleasant
+worthless".Split('\n');
 
     void Start ()
 	{
@@ -96,10 +133,22 @@ terrific".Split('\n');
     {
         while (isGameInProgress)
         {
-            var go = Instantiate(GoodTextPrefab, TextParent, false);
-            go.transform.localPosition = new Vector3(Random.Range(-200, 200), 900, 0);
-            go.name = GoodTextPrefab.name + " instantiated";
-            go.GetComponent<UnityEngine.UI.Text>().text = goodWords[Random.Range(0, goodWords.Length - 1)];
+            Bias = Random.Range(1, 100);
+            if(Bias >= 30)
+            {
+                var go = Instantiate(GoodTextPrefab, TextParent, false);
+                go.transform.localPosition = new Vector3(Random.Range(-200, 200), 900, 0);
+                go.name = GoodTextPrefab.name + " instantiated";
+                go.GetComponent<UnityEngine.UI.Text>().text = goodWords[Random.Range(0, goodWords.Length - 1)];
+            }
+            else
+            {
+                var go = Instantiate(BadTextPrefab, TextParent, false);
+                go.transform.localPosition = new Vector3(Random.Range(-200, 200), 900, 0);
+                go.name = BadTextPrefab.name + " instantiated";
+                go.GetComponent<UnityEngine.UI.Text>().text = badWords[Random.Range(0, goodWords.Length - 1)];
+            }
+            
             yield return new WaitForSeconds(1.0f);
         }
     }
