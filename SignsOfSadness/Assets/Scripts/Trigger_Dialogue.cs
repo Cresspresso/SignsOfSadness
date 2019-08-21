b5e9b4dce2e9fd9193aba882b5fe7476a60148df
1Angelika <53497909+1Angelika@users.noreply.github.com>
2019-08-22 02:24:51 +1200

    s

--- SignsOfSadness/Assets/Scripts/Trigger_Dialogue.cs
+++ SignsOfSadness/Assets/Scripts/Trigger_Dialogue.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trigger : MonoBehaviour {
+public class Trigger_Dialogue : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject UIObject;
+    void Start()
+    {
+        UIObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            UIObject.SetActive(true);
+
+        }
+
+    }
 }
